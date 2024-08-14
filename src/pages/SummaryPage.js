6b9aab273c21d9d17d1cedc7ab9971c90e8341b9
Omkar Raut <120701365+Omkar3551@users.noreply.github.com>
2024-08-14 20:52:38 +0530

    Update SummaryPage.js

--- src/pages/SummaryPage.js
+++ src/pages/SummaryPage.js
@@ -34,7 +34,7 @@ export default function SummaryPage() {
 }, []);
 
 const loadProducts=async ()=>{
-  const result=await axios.get("https://radiant-unity-production.up.railway.app/products"); //link from postman get method
+  const result=await axios.get("https://springbootbackend-production-555f.up.railway.app/products"); //link from postman get method
   setProducts(result.data);
 };
 
